d60bee2a197241bd8a1f4722c038e13b1f360076
Niclas Rosengren <niclas.rosengren@gmail.com>
2019-05-21 17:17:27 +0200

    added support for boundaries without leading crlf

--- src/parser.rs
+++ src/parser.rs
@@ -70,8 +70,12 @@ pub(crate) struct BoundaryParser {
 
 impl BoundaryParser {
     pub fn with_capacity<S: AsRef<str>>(boundary: S, capacity: usize) -> Self {
+        let boundary = format!("--{}", boundary.as_ref());
+
+        log::debug!("Creating with boundary: {:?}", boundary);
+
         Self {
-            boundary: format!("\r\n--{}", boundary.as_ref()),
+            boundary,
             buffer: BytesMut::with_capacity(capacity),
         }
     }
@@ -84,6 +88,9 @@ impl BoundaryParser {
         if self.buffer.len() < boundary.len() {
             return ParseResult::NotReady;
         }
+
+        // normally there are no preambles, so we could skip this search by just checking if the initial bytes
+        // equals our boundary. To `optimize` the common case.
 
         // Find the start, might have to skip the preamble. It is to be discarded.
         let mut part_start = match twoway::find_bytes(&self.buffer, boundary) {
@@ -101,7 +108,11 @@ impl BoundaryParser {
                 part_start += 2;
             }
 
-            BOUNDARY_LAST_PART_SENTINEL => return ParseResult::Done,
+            BOUNDARY_LAST_PART_SENTINEL => {
+                log::debug!("Found stop sentinel at index: {}", part_start);
+
+                return ParseResult::Done;
+            }
 
             slice => {
                 return ParseResult::Err(Error::malformed(format!(
@@ -116,7 +127,8 @@ impl BoundaryParser {
                 // We've found an entire part, snap it of and return it.
 
                 self.buffer.advance(part_start);
-                let part_bs = self.buffer.split_to(i).freeze();
+                let part_bs = self.buffer.split_to(i - 2).freeze();
+                self.buffer.advance(2); // advance past the leading crlf in the next part.
                 ParseResult::Ready(part_bs)
             }
 
@@ -132,6 +144,66 @@ mod tests {
 
     #[test]
     fn parse_simple_boundary() {
+        let data = "\r
+\r
+--simple boundary\r
+\r
+Part1\r
+--simple boundary\r
+Content-type: text/plain; charset=us-ascii\r
+\r
+Part2\r
+\r
+--simple boundary--\r
+";
+
+        let mut p = BoundaryParser::with_capacity("simple boundary", 500);
+
+        let exp = "\r
+Part1";
+
+        assert_eq!(ParseResult::Ready(exp.into()), p.parse(data));
+
+        let exp = "Content-type: text/plain; charset=us-ascii\r
+\r
+Part2\r
+";
+
+        assert_eq!(ParseResult::Ready(exp.into()), p.parse(data));
+        assert_eq!(ParseResult::Done, p.parse(data));
+    }
+
+    #[test]
+    fn parse_boundary_without_leading_crlf() {
+        let data = "--simple boundary\r
+\r
+Part1\r
+--simple boundary\r
+Content-type: text/plain; charset=us-ascii\r
+\r
+Part2\r
+\r
+--simple boundary--\r
+";
+
+        let mut p = BoundaryParser::with_capacity("simple boundary", 500);
+
+        let exp = "\r
+Part1";
+
+        assert_eq!(ParseResult::Ready(exp.into()), p.parse(data));
+
+        let exp = "Content-type: text/plain; charset=us-ascii\r
+\r
+Part2\r
+";
+
+        assert_eq!(ParseResult::Ready(exp.into()), p.parse(data));
+        assert_eq!(ParseResult::Done, p.parse(data));
+    }
+
+    #[test]
+    fn parse_boundary_with_preamble() {
         let data = "\r
 \r
 This is the preamble.  It is to be ignored, though it\r
